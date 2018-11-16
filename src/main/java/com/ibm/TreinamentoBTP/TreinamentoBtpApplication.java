d503a7447a8d8a6f25dbba7bf756732ac3f9e781
Taila Saviani <taila.nayara.saviani@ibm.com>
2018-11-16 15:28:54 -0200

    Initial commit

---
+++ src/main/java/com/ibm/TreinamentoBTP/TreinamentoBtpApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.TreinamentoBTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoBtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoBtpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ibm/TreinamentoBTP/TreinamentoBtpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.TreinamentoBTP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreinamentoBtpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
